747b819841fba09914000909dc9274105820b427
syedincharge <rizvisyedba@gmail.com>
2022-05-22 14:27:49 -0600

    second commit

--- src/com/rizvi/lamda/RunnableExample.java
+++ src/com/rizvi/lamda/RunnableExample.java
@@ -2,48 +2,37 @@ package com.rizvi.lamda;
 
 public class RunnableExample {
 
-    // Runnable Traditional example
-
     public static void main(String[] args) {
-
+        // Runnable Traditional example
         Runnable runnable = new Runnable() {
-
             @Override
             public void run() {
-
                 int sum = 0;
-                for(int i = 0; i < 10; i++)
+                for(int i = 0; i <= 10; i++)
                     sum += i;
-                System.out.println("Traditional:  "+ sum);
+                System.out.println("Traditional way:   "+ sum);
             }
         };
-
         new Thread(runnable).start();
 
-        // Implement using Lambda
+         // Implement using Lambda
 
         Runnable runnable1 = () -> {
-
-            int sum = 0;
-            for (int i = 0; i < 10; i++) {
-               sum += i;
-                System.out.println("Runnable Lambda :  "+sum);
-            }
-        };
-
-       new Thread(runnable1).start();
-
-     // Implement using Thread with Lambda
-
-      new Thread(() -> {
-
           int sum = 0;
-          for (int i = 0; i < 10 ; i++) {
-              sum += i;
-              System.out.println("Thread with Lambda " +sum);
-          }
+            for (int i = 0; i <= 10; i++)
+                 sum += i;
+            System.out.println("Runnable Lambda :  "+sum);
+        };
+        new Thread(runnable1).start();
 
-      }).start();
 
-    }
+    //     Implement using Thread with Lambda
+        new Thread(() -> {
+            int sum = 0;
+            for (int i = 0; i <= 10; i++)
+                sum += i;
+            System.out.println("Thread with Lambda " + sum);
+         }).start();
+
+       }
 }
